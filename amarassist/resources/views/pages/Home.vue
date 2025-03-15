dfb97634deb07a35f78de68ccdcd6668765bd6b4
joseguilhermeromano <jose_guilherme_romano@hotmail.com>
2025-03-15 17:21:50 -0300

    feat: adding vue3 with vite and inertiajs in the Laravel 9.

---
+++ amarassist/resources/views/pages/Home.vue
@@ -0,0 +1,22 @@
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "HelloWorld",
+    data() {
+        return {
+            message: "Hello World!",
+        };
+    },
+});
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
