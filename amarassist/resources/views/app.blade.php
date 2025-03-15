ae9c8874599130ca3751b44674527c2489254d8f
joseguilhermeromano <jose_guilherme_romano@hotmail.com>
2025-03-15 19:26:09 -0300

    feat: adding favicon and renaming title tab.

--- amarassist/resources/views/app.blade.php
+++ amarassist/resources/views/app.blade.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="icon" href="{{ asset('/img/favicon.png') }}" type="image/x-icon">
+    <title>AMAR ASSIST</title>
     @vite(['resources/js/app.js', 'resources/scss/app.scss'])
     @inertiaHead
   </head>
